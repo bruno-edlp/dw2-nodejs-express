9566e3e365903cae75bcf85a67d8dcf97659183c
DSM2 <fatec-dsm2@academico.intra>
2025-02-13 19:22:15 -0300

    Variáveis e funções no JS

---
+++ aula0.1-variaveis-e-funcoes/js-aula-0.1.js
@@ -0,0 +1,11 @@
+"use strict";
+//Comentario
+/* Comentario em bloco
+CTRL + ; = atalho pq comentar
+ */
+
+const nome = "Bruno"; //Atribuir valor 
+let Nome; // Pode começar vazia
+
+const message = "Hello World nesse caraio!";
+console.log(message);
